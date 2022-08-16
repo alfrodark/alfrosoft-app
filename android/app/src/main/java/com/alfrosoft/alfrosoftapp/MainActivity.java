73c8656501e8221c84bbd7820f2595474c0d6228
alfrodark <alfrodarrk@gmail.com>
2022-08-16 20:58:39 +0000

    first commit

---
+++ android/app/src/main/java/com/alfrosoft/alfrosoftapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.alfrosoft.alfrosoftapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alfrosoft.alfrosoftapp',
+  appName: 'alfrosoft-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
